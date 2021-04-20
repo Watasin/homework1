ab133f66d7e68a7c4490b2d62da360612cb1d0e3
LAPTOP-DI30GGAL\iiisinnnn <watasin.kongkhun@mail.kmutt.ac.th>
2021-04-20 14:38:49 +0700

    คำตอบข้อที่ 1

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            while (check != true)
+            {
+                Console.Write("Input Number : ");
+                int row = int.Parse(Console.ReadLine());
+                if (row >= 0)
+                {
+                    Row(row);
+                    check = true;
+                }
+                else
+                    Console.WriteLine("Invalid Pascal’s triangle row number.");
+            }
+        }
+        
